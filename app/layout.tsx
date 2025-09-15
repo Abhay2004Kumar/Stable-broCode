cfde9c9dd3f0b6cf642c8c3b65c2dba140198ddd
Abhay Kumar <119209682+Abhay2004Kumar@users.noreply.github.com>
2025-09-16 01:04:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "BroCode - Editor",
-  description: "BroCode - Editor - Code Editor For BroCoders is a free online code editor that lets you write, debug, and run your code in the browser. It is an open source editor that is easy to use and has a simple interface. It is also a great way to learn programming and get started with coding.",
+  description: "BroCode - Editor - BroCoders is a free online code editor that lets you write, debug, and run your code in the browser. It is an open source editor that is easy to use and has a simple interface. It is also a great way to learn programming and get started with coding.",
 };
 
 export default async function RootLayout({
