4b2dcd67a649097301cc573b09e685caf900958b
Abhay2004Kumar <abs220502@gmail.com>
2025-09-19 15:08:52 +0530

    added react-tailwind template

--- lib/template.ts
+++ lib/template.ts
@@ -1,6 +1,6 @@
 export const templatePaths = {
-    REACT: 'starters/react-ts',
-    NEXTJS: 'starters/nextjs',
+    REACT: 'starters/react-tailwind',
+    NEXTJS: 'starters/nextjs-shadcn',
     EXPRESS: 'starters/express-simple',
     VUE: 'starters/vue',
     HONO: 'starters/hono-nodejs-starter',
