a10ff19c42c5a67b27088b4daf9cadeabf3b09d1
Abhay Kumar <119209682+Abhay2004Kumar@users.noreply.github.com>
2026-01-08 18:38:24 +0530

    Remove Pricing link from header
    Removed the Pricing link from the header component.

--- features/home/header.tsx
+++ features/home/header.tsx
@@ -53,12 +53,7 @@ export function Header() {
                     > */}
                       {/* Portfolio */}
                     {/* </Link> */}
-                    <Link
-                                            href="/pricing"
-                                            className="text-sm text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100 transition-colors"
-                                        >
-                                            Pricing
-                                        </Link> 
+                    
                   
                   </div>
                 </div>
